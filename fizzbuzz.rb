26a962d7cf658a48eada7fa7b6ff47cc87b8cdba
Ally Netzel <netzelally@gmail.com>
2018-03-22 02:58:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(input)
   if input % 3 == 0 
     return "Fizz"
   end
-  if input % 5 == 0 
+  elsif input % 5 == 0 
     return "Buzz"
   end
-  if input % 5 == 0 || input % 3 == 0 
+  elsif input % 5 == 0 || input % 3 == 0 
     return "Fizzbuzz"
   end
   else input % 5 != 0 || input % 3 != 0 
