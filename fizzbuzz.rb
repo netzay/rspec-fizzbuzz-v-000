71ca3d9d91055223fe74211234b0275d9ca97aa7
Ally Netzel <netzelally@gmail.com>
2018-03-22 02:47:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0 
+    print "Fizz"
+  if int % 5 == 0 
+    print "Buzz"
+  if int % 5 == 0 || int % 3 == 0  
+    print "FizzBuzz"
+  else
+    print "nil"
   
 end
