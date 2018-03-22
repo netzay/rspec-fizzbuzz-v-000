33926407cb2f53db0be6f39c4e0f11b38db313ce
Ally Netzel <netzelally@gmail.com>
2018-03-22 02:49:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 3 == 1  
     puts "Fizz"
-  if int % 5 == 0 
+  if int % 5 == 1 
     puts "Buzz"
-  if int % 5 == 0 || int % 3 == 0  
+  if int % 5 == 1 || int % 3 == 1   
     puts "FizzBuzz"
   else
     puts "nil"
